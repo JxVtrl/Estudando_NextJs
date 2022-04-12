593c042ef2409920add1985921360eb6891b5170
Joao <joaoviniciusvitral@hotmail.com>
2022-04-12 11:56:19 -0300

    feat

--- pages/hello/[slug]/idade/index.tsx
+++ pages/hello/[slug]/idade/index.tsx
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+
 const Idade = () => {
+    const router = useRouter();
+    const { slug } = router.query;
+
     return (
         <div>
-        <h1>Idade</h1>
+            <h1>{slug} tem 22 anos</h1>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             doloremque, quidem, quisquam doloremque.
